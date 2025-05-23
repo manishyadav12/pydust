685e2e6b33a0d7ddf67cc29094227735ddcef10c
manishkumar.yadav_csgi <manishkumar.yadav@csgi.com>
2025-05-24 01:45:26 +0530

    22-may-practice

---
+++ 2.py
@@ -0,0 +1,11 @@
+# name = input("What is your name: ")
+#
+# print("My Name is " +  name)
+# name = input("What is your name: ")
+# print("My Name is",name)
+
+# to override default new line feature of print(), use print(end='' )
+
+name = input("What is your name: ")
+
+print(f"Hello, {name}")  # f is used to formate string
